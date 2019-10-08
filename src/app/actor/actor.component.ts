780aba33a473a93187aa2ecc6b0717950aa57a07
zche0034 <zche0034@student.monash.edu>
2019-10-08 11:38:25 +1100

    Initial Commit

---
+++ src/app/actor/actor.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from "@angular/core";
+import { DatabaseService } from "../database.service";
+
+@Component({
+  selector: "app-actor",
+  templateUrl: "./actor.component.html",
+  styleUrls: ["./actor.component.css"],
+})
+export class ActorComponent implements OnInit {
+  actorsDB: any[] = [];
+  moviesDB: any[] = [];
+
+  section = 1;
+
+  fullName: string = "";
+  bYear: number = 0;
+  actorId: string = "";
+
+  title: string = "";
+  year: number = 0;
+  movieId: string = "";
+
+  constructor(private dbService: DatabaseService) {}
+
+  //Get all Actors
+  onGetActors() {
+    this.dbService.getActors().subscribe((data: any[]) => {
+      this.actorsDB = data;
+    });
+  }
+  //Create a new Actor, POST request
+  onSaveActor() {
+    let obj = { name: this.fullName, bYear: this.bYear };
+    this.dbService.createActor(obj).subscribe(result => {
+      this.onGetActors();
+    });
+  }
+  // Update an Actor
+  onSelectUpdate(item) {
+    this.fullName = item.name;
+    this.bYear = item.bYear;
+    this.actorId = item._id;
+  }
+  onUpdateActor() {
+    let obj = { name: this.fullName, bYear: this.bYear };
+    this.dbService.updateActor(this.actorId, obj).subscribe(result => {
+      this.onGetActors();
+    });
+  }
+
+  //Delete Actor
+  onDeleteActor(item) {
+    this.dbService.deleteActor(item._id).subscribe(result => {
+      this.onGetActors();
+    });
+  }
+  // This lifecycle callback function will be invoked with the component get initialized by Angular.
+  ngOnInit() {
+    this.onGetActors();
+  }
+
+  changeSection(sectionId) {
+    this.section = sectionId;
+    this.resetValues();
+  }
+
+  resetValues() {
+    this.fullName = "";
+    this.bYear = 0;
+    this.actorId = "";
+  }
+
+  //Get all Movies
+  onGetMovies() {
+    this.dbService.getMovies().subscribe((data: any[]) => {
+      this.moviesDB = data;
+    });
+  }
+  //Create a new Movie, POST request
+  onSaveMovie() {
+    let obj = { title: this.title, year: this.year };
+    this.dbService.createMovie(obj).subscribe(result => {
+      this.onGetMovies();
+    });
+  }
+  //Delete Movie
+  onDeleteMovie(item) {
+    this.dbService.deleteMovie(item._id).subscribe(result => {
+      this.onGetMovies();
+    });
+  }
+  //Delete All
+  onDeleteYear(time) {
+    this.onGetMovies();
+    let i = 0;
+    while (i < this.moviesDB.length){
+      if (this.moviesDB[i].year >= time) {
+        this.moviesDB.splice(i,1);
+      }else{ i++; }
+    }
+    for (let j=0; j<this.moviesDB.length; j++) {
+      this.dbService.deleteMovie(this.moviesDB[j]._id).subscribe(result => {
+        this.onGetMovies();
+      });
+    }
+  }
+  //Add actor to movie
+  onActorMovie(item) {
+    this.dbService.updateActor(this.actorId, item).subscribe(result => {
+      this.onGetActors();
+    });
+  }
+}
+
